9a6c8dbb283c1d2da744eb85f05ab83090f70f8a
Tetsuya Mori <monry84@gmail.com>
2018-01-19 12:38:55 +0900

    :sparkles: Fix resolve to path to dg command

--- Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UnityModule.Settings {
 
@@ -8,23 +9,34 @@ namespace UnityModule.Settings {
         // ReSharper disable once PartialTypeWithSinglePart
         public partial class EnvironmentSetting_Path {
 
-            private const string DEFAULT_DEPLOY_GATE_COMMAND_PATH = "";
+            /// <summary>
+            /// デフォルトの dg コマンドパス
+            /// </summary>
+            private const string DEFAULT_COMMAND_PATH_DEPLOY_GATE = "/usr/local/bin/dg";
+
+            /// <summary>
+            /// dg コマンドへのパスを保存している環境変数のキー
+            /// </summary>
+            private const string ENVIRONMENT_KEY_COMMAND_DEPLOY_GATE = "COMMAND_DEPLOY_GATE";
 
             /// <summary>
             /// dg コマンドのパスの実体
             /// </summary>
             [SerializeField]
-            private string commandDeployGate = DEFAULT_DEPLOY_GATE_COMMAND_PATH;
+            private string commandDeployGate;
 
             /// <summary>
             /// dg コマンドのパス
             /// </summary>
             public string CommandDeployGate {
                 get {
+                    if (string.IsNullOrEmpty(this.commandDeployGate)) {
+                        this.commandDeployGate = Environment.GetEnvironmentVariable(ENVIRONMENT_KEY_COMMAND_DEPLOY_GATE);
+                    }
+                    if (string.IsNullOrEmpty(this.commandDeployGate)) {
+                        this.commandDeployGate = DEFAULT_COMMAND_PATH_DEPLOY_GATE;
+                    }
                     return this.commandDeployGate;
-                }
-                set {
-                    this.commandDeployGate = value;
                 }
             }
 
